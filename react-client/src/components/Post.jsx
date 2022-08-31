69494a7fd05bba07a932583264de7e6f7df2daec
sgallagher <samuel.gallagher@cengage.com>
2022-08-31 08:26:04 -0400

    Updating to material UI Typography and Button

--- react-client/src/components/Post.jsx
+++ react-client/src/components/Post.jsx
@@ -28,16 +28,16 @@ function Post(props) {
       event.preventDefault();
     }
 
-    return <Card sx={{ maxWidth: 345 }}>
+    return <Card sx={{ maxWidth: 345 }} variant="outlined">
     <CardContent>
-    <h5>{props.title}</h5>
-    <div>
+    <Typography variant="h5">{props.title}</Typography>
+    <Typography variant="body2">
       {props.content}
-    </div>
+    </Typography>
     </CardContent>
     <CardActions>
-    {props.status === 'backlog' ? <button>Send to In Progress</button> : props.status === 'in-progress'? <button>Send Complete</button> : ''}
-    <button onClick={handleClick}>Delete</button>
+    {props.status === 'backlog' ? <Button variant="contained">Send to In Progress</Button> : props.status === 'in-progress'? <button>Send Complete</button> : ''}
+    <Button variant="outlined" onClick={handleClick}>Delete</Button>
     </CardActions>
   </Card>;
 }

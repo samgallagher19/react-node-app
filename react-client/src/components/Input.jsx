2c40710e71d265bce1bc6f129eb6ea5f2e63fed6
sgallagher <samuel.gallagher@cengage.com>
2022-08-31 14:57:20 -0400

    Updating submit button

--- react-client/src/components/Input.jsx
+++ react-client/src/components/Input.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 
 function Input(props) {

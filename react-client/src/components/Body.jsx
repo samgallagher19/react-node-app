f5a54d7b908596c1d05327b190310f3d21245f9b
sgallagher <samuel.gallagher@cengage.com>
2022-08-31 09:44:20 -0400

    Adding Grid layout from material ui

--- react-client/src/components/Body.jsx
+++ react-client/src/components/Body.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Post from "./Post";
-
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
 
 function Body(props) {
@@ -9,7 +10,11 @@ function Body(props) {
       
   console.log(props.posts);
 
-  return <div>{props.posts.map(post => <Post title={post.title} content={post.content} status={post.status} key={post._id} id={post._id} onUpdatePosts={props.onUpdatePosts}/>)}</div>;
+  return <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={2}>
+            {props.posts.map(post => <Grid item xs={8}><Post title={post.title} content={post.content} status={post.status} key={post._id} id={post._id} onUpdatePosts={props.onUpdatePosts}/></Grid>)}
+          </Grid>
+        </Box>;
 }
 
 export default Body;

08fed0d025d81a591a83d25d3e8e161cb2ad5709
sgallagher <samuel.gallagher@cengage.com>
2022-08-31 11:01:21 -0400

    Adding margin to Box

--- react-client/src/components/Body.jsx
+++ react-client/src/components/Body.jsx
@@ -10,7 +10,7 @@ function Body(props) {
       
   console.log(props.posts);
 
-  return <Box sx={{ flexGrow: 1 }}>
+  return <Box sx={{ flexGrow: 1, marginTop: "1em" }}>
           <Grid container spacing={2}>
             {props.posts.map(post => <Grid item xs={4}><Post title={post.title} content={post.content} status={post.status} key={post._id} id={post._id} onUpdatePosts={props.onUpdatePosts}/></Grid>)}
           </Grid>
